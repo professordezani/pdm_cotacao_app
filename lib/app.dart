62b51331bf111cd37e96ec125ef0835501171bce
Henrique Dezani <professorhenriquedezani@gmail.com>
2025-04-04 17:54:07 -0300

    Projeto finalizado

--- lib/app.dart
+++ lib/app.dart
@@ -2,29 +2,69 @@
 // ignore_for_file: prefer_const_literals_to_create_immutables
 
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
 
 class CotacaoApp extends StatelessWidget {
+
+  var txtReais = TextEditingController();
+  var txtResultado = TextEditingController();
+  String moeda = "USD-BRL";
+
+  void converter() async {
+    String url = "https://economia.awesomeapi.com.br/json/last/$moeda";
+
+    var response = await http.get(Uri.parse(url));
+
+    Map<String, dynamic> dados = json.decode(response.body);
+
+    double cotacao = double.parse(dados[moeda.replaceFirst("-", "")]["ask"]);
+
+    var resultado = double.parse(txtReais.text) / cotacao;
+    txtResultado.text = resultado.toString();
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(),
         body: Container(
-          margin: EdgeInsets.all(20),
+          margin: EdgeInsets.all(10),
           child: Column(
+            //spacing: 10,
             children: [
               TextField(
+                controller: txtReais,
                 keyboardType: TextInputType.number,
                 decoration: InputDecoration(
                   labelText: "Reais",
                   border: OutlineInputBorder(),
                 ),
               ),
+              Container(height: 10,),
+              DropdownMenu(
+                onSelected: (value) => moeda = value!,
+                dropdownMenuEntries: [
+                  DropdownMenuEntry(value: "USD-BRL", label: "Dólar"),
+                  DropdownMenuEntry(value: "EUR-BRL", label: "Euro"),
+                  DropdownMenuEntry(value: "BTC-BRL", label: "Bitcoin")
+                ],
+              ),
+              Container(height: 10,),
               ElevatedButton(
-                onPressed: null,
+                onPressed: converter,
                 child: Text("Converter"),
               ),
-              Text("Dólares")
+              Container(height: 10,),
+              TextField(
+                controller: txtResultado,
+                keyboardType: TextInputType.number,
+                decoration: InputDecoration(
+                  labelText: "Resultado",
+                  border: OutlineInputBorder(),
+                ),
+              ),
             ],
           ),
         ),
